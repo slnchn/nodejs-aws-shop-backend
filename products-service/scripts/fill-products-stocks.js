7d80b03aec5787f1c5a45f527392980ac45e118a
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-11-25 12:39:01 +0100

    add filling tables with mock data

---
+++ products-service/scripts/fill-products-stocks.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
+
+// data
+const products = require("./products-data.json");
+const stocks = require("./stocks-data.json");
+
+dotenv.config();
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const dynamoDb = DynamoDBDocument.from(client);
+
+const putData = async (table, items) => {
+  items.forEach(async (item) => {
+    const params = {
+      TableName: table,
+      Item: item,
+    };
+
+    try {
+      dynamoDb.put(params, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+putData(process.env.PRODUCTS_TABLE_NAME, products);
+putData(process.env.STOCKS_TABLE_NAME, stocks);
